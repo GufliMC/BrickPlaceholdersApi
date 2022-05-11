2fe4ff0390c11616bbfbf6027022e413e44c1fc7
Joris Guffens <joris.guffens@outlook.com>
2022-05-11 18:01:56 +0200

    updated build files

--- minestom-api/src/main/java/com/guflimc/brick/placeholders/minestom/api/MinestomPlaceholderManager.java
+++ minestom-api/src/main/java/com/guflimc/brick/placeholders/minestom/api/MinestomPlaceholderManager.java
@@ -1,31 +1,8 @@
 package com.guflimc.brick.placeholders.minestom.api;
 
 import com.guflimc.brick.placeholders.api.PlaceholderManager;
-import net.kyori.adventure.text.Component;
 import net.minestom.server.entity.Player;
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public interface MinestomPlaceholderManager extends PlaceholderManager<Player> {
 
-    @Override
-    Component replace(@NotNull Player entity, @NotNull Component component);
-
-    @Override
-    Component replace(@NotNull Player entity, @NotNull String text);
-
-    @Override
-    Collection<String> placeholders();
-
-    @Override
-    void registerReplacer(@NotNull String key, @NotNull BiFunction<Player, String, Component> replacer);
-
-    @Override
-    void registerReplacer(@NotNull String key, @NotNull Function<Player, Component> replacer);
-
-    @Override
-    void unregisterReplacer(@NotNull String key);
 }
